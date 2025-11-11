2086ad98199ea91972df961d017a8395389db89a
MisaelZanetti <misaelzanetti@gmail.com>
2025-11-11 13:38:33 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/Inicio.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routes>
+      <Route path='/' element={<Inicio />}></Route>
+    </Routes>
   </StrictMode>,
 )
